1cf5e62b5a7aff2321e731e23a15b5f8261da9a1
laughingbiscuit <sean@laughingbiscuit.com>
2022-12-04 09:15:02 +0000

    day 4 part 2

---
+++ day4/part2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# how many pairs does one range full contain the other?
+
+cat input.txt | awk -F, '
+BEGIN { total=0 }
+{
+  split($1, left, "-")
+  split($2, right, "-")
+  if( ! (left[2] < right [1] ||
+        right[2] < left[1]) ) {
+    total+=1
+  }}
+END { print total }
+'
